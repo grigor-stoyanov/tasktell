9d0a67a2775a55502248185e5696a2e328847f02
unknown <grigor.stoyanov@hotmail.com>
2022-04-27 15:46:36 +0300

    added staticfiles root

--- tasktell/settings.py
+++ tasktell/settings.py
@@ -142,6 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 BASE_DIR2 = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = 'static/'
+STATIC_ROOT=os.path.join(BASE_DIR2, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR2, 'static'),)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
