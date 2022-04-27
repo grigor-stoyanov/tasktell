af07531483f046be5721649fe8aecd5d8c9ede1a
unknown <grigor.stoyanov@hotmail.com>
2022-04-27 15:58:03 +0300

    changed database setting to heroku postgresql

--- tasktell/settings.py
+++ tasktell/settings.py
@@ -81,16 +81,28 @@ WSGI_APPLICATION = 'tasktell.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'tasktell_db',
+#         'USER': 'postgres',
+#         'PASSWORD': '1123QwER',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tasktell_db',
-        'USER': 'postgres',
-        'PASSWORD': '1123QwER',
-        'HOST': 'localhost',
+        'NAME': 'd7i0s6bicf5g9i',
+        'USER': 'hnjoocwzadxmdd',
+        'PASSWORD': '2eb1d739da4154ad197810a9df0e2043957320541a7b32e25fe9330037c5fc8a',
+        'HOST': 'ec2-99-80-170-190.eu-west-1.compute.amazonaws.com',
         'PORT': '5432',
     }
 }
+
 LOGGING = {
     'version': 1,
     'filters': {
