85d90a5c8dad1b73803792209da13cca212944e4
unknown <grigor.stoyanov@hotmail.com>
2022-04-27 15:07:19 +0300

    added whitenoise for serving staticfiles on heroku

--- tasktell/settings.py
+++ tasktell/settings.py
@@ -43,6 +43,7 @@ TASKTELL_APPS = [
 INSTALLED_APPS = DJANGO_APPS + TASKTELL_APPS
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -144,8 +145,10 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
-MEDIA_URL= 'media/'
+MEDIA_URL = 'media/'
 
 AUTH_USER_MODEL = "auth_app.TasktellUser"
 
