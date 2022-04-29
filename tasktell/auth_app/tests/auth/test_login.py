33588b6e3c075f29156f650c19cac8bcc6e89ff4
unknown <grigor.stoyanov@hotmail.com>
2022-04-29 17:28:02 +0300

    added dotenv to requirements txt

--- tasktell/auth_app/tests/auth/test_login.py
+++ tasktell/auth_app/tests/auth/test_login.py
@@ -2,5 +2,7 @@ from django.test import TestCase, Client
 
 
 class TestLogin(TestCase):
+    def setUp(self) -> None:
+        pass
     def test_login_successful_and_redirect_to_home(self):
-        
+        pass
