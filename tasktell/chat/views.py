4bac509289d4a46364dfc044b063645b1c6bb890
unknown <grigor.stoyanov@hotmail.com>
2022-05-20 16:48:27 +0300

    added realtime support for chat model with websockets and django channels

--- tasktell/chat/views.py
+++ tasktell/chat/views.py
@@ -5,6 +5,7 @@ from django.views.generic.edit import FormMixin
 
 from tasktell.chat.forms import SendMessageForm
 from tasktell.chat.models import Chat, Messages
+from tasktell.common.helpers import get_logged_in_user_as_member_or_none
 from tasktell.main.models import Project
 
 
@@ -20,6 +21,8 @@ class ChatDetailsView(LoginRequiredMixin, FormMixin, DetailView):
         initial = super().get_initial()
         initial['created_by'] = self.request.user
         initial['sent_to'] = Chat.objects.get(pk=self.kwargs['id'])
+        initial['sent_from'] = Chat.objects.get(
+            members_id=get_logged_in_user_as_member_or_none(self.kwargs['pk'], self.request.user.pk))
         return initial
 
     def get_context_data(self, **kwargs):
@@ -27,11 +30,13 @@ class ChatDetailsView(LoginRequiredMixin, FormMixin, DetailView):
         current_chat = Chat.objects.get(pk=self.kwargs['id'])
         user_member = self.object.member_set.select_related().get(projects=self.kwargs['pk'],
                                                                   user_id=self.request.user.pk)
-        my_chat = Chat.objects.get(members_id=user_member.pk)
-        context['chat_list'] = Chat.objects.exclude(members_id=user_member.pk)
+        my_chat = Chat.objects.get(members_id=user_member.pk, project_id=self.kwargs['pk'])
+        context['chat_list'] = Chat.objects.filter(project_id=self.kwargs['pk']).exclude(members_id=user_member.pk)
         context['current_chat'] = current_chat
-        received_messages = Messages.objects.filter(chat_id=my_chat.pk, created_by=current_chat.members.user.pk)
-        sent_messages = Messages.objects.filter(chat_id=current_chat.pk, created_by=self.request.user.pk)
+        received_messages = Messages.objects.select_related().filter(chat_id=my_chat.pk,
+                                                                     created_by=current_chat.members.user.pk)
+        sent_messages = Messages.objects.select_related().filter(chat_id=current_chat.pk,
+                                                                 created_by=self.request.user.pk)
         context['messages'] = received_messages | sent_messages
         return context
 
